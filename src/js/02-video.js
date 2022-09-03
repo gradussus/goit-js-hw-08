45d5b3ba093eb5a925b4461070249cb3f7092128
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-09-03 19:14:09 +0300

    Вроде готово

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('pause', function () {
+  console.log('paused the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+
+filling();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: form.email.value, message: form.message.value })
+  );
+}
+
+function filling() {
+  if (localStorage.getItem('feedback-form-state')) {
+    const memory = JSON.parse(localStorage.getItem('feedback-form-state'));
+    form.email.value = memory.email;
+    form.message.value = memory.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log({ email: form.email.value, message: form.message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
