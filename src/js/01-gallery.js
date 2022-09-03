303fb58a530ca317900678836ca290bddcf8d71a
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-09-03 12:11:49 +0300

    gg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryEl);
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
